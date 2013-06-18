0294ff7f73e0b2603ca9fb8247e776df8e90188a
jp <gonjuanp@gmail.com>
2013-06-17 20:42:58 -0500

    Version 1.0
    Mongoid gem first error

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongo::Application.config.secret_token = 'ce8a1bdd6c38e676e22d773fcedf5d8d6cadeb74d0dce67e71ec154673f34836c30d3b8198ccd59d4e9a45407f38f8d3c0dab73d3ba41f2b3ea9cc4289687b55'
